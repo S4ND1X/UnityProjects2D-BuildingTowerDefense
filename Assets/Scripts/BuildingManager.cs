1888bcc81a076e066167b27811d7768033aff9a7
S4NDIX <sandix390@gmail.com>
2020-09-17 18:20:16 -0500

    Starting with building manager
    Added script to get mouse click position relative to the screen and instantiate a prefab

---
+++ Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour{
+
+    //Cached references
+    [SerializeField] private Camera mainCamera;
+
+    [SerializeField] private Transform pfWoodHarvester;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(pfWoodHarvester, GetMouseWorldPosition(), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    private Vector3  GetMouseWorldPosition(){
+        Vector3 mouseWorldPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPosition.z = 0f;
+        return mouseWorldPosition;
+    }
+}
